5c36ddf9f50abe760204effa25c7d25276556d6d
bwire <ahmedbwire17@gmail.com>
2025-03-11 06:51:28 +0300

    Adds js function continue

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+// 1. Defines saturdayFun function declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// 2. Defines mondayWork function expression
+const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+// 3. Defines wrapAdjective function
+function wrapAdjective(wrapper = '*') {
+    return function(adjective = 'special') {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+}
